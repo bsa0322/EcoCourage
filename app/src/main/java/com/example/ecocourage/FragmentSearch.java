88dbbd7c492fab081c96566aed66b29ddfd94db4
suabae <bsa0322@ewhain.net>
2021-05-25 04:24:21 +0900

    현위치 기능 추가

--- app/src/main/java/com/example/ecocourage/FragmentSearch.java
+++ app/src/main/java/com/example/ecocourage/FragmentSearch.java
@@ -5,12 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.util.FusedLocationSource;
 
-public class FragmentSearch extends Fragment {
-    private MapView mapView;
+public class FragmentSearch extends Fragment implements OnMapReadyCallback {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+    private MapView mapView; //지도
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
@@ -19,7 +28,26 @@ public class FragmentSearch extends Fragment {
         mapView = v.findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
 
+        mapView.getMapAsync(this);
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+
         return v;
     }
 
+    public void OnRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)){
+            if(!locationSource.isActivated()) {//권한 거부됨
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+    }
 }
