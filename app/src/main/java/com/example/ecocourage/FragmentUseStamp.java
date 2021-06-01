62722811204d66d77ca04e2cde80f1e4cc8f4717
jaeserrr@ewhain.net <jaeserrr@ewhain.net>
2021-06-02 03:03:26 +0900

    버튼 구현 전까지(리사이클러뷰, 리스트각각 창)

--- app/src/main/java/com/example/ecocourage/FragmentUseStamp.java
+++ app/src/main/java/com/example/ecocourage/FragmentUseStamp.java
@@ -5,9 +5,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class FragmentUseStamp extends Fragment {
+
+    private ArrayList<UseStampStoreData> arrayList;
+    private UseStampAdapter useStampAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+
+        recyclerView =(RecyclerView)getActivity().findViewById(R.id.usestampstore);
+        linearLayoutManager=new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        arrayList =new ArrayList<>();
+
+        useStampAdapter =new UseStampAdapter(arrayList);
+        recyclerView.setAdapter(useStampAdapter);
+
         return inflater.inflate(R.layout.fragment_usestamp,container,false);
     }
 }
