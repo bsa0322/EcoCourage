d58f6c6e4684d945faa2e3f8f783d9c489785936
jaeserrr@ewhain.net <jaeserrr@ewhain.net>
2021-06-13 21:53:20 +0900

    애용카드 효과, 테마 이것저것

--- app/src/main/java/com/example/ecocourage/FragmentAeyoungCard.java
+++ app/src/main/java/com/example/ecocourage/FragmentAeyoungCard.java
@@ -4,11 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 public class FragmentAeyoungCard extends Fragment {
+    Button aeyoungBtn;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_aeyoungcard,container,false);
+        View v= inflater.inflate(R.layout.fragment_aeyoungcard,container,false);
+        
+        Animation animation = AnimationUtils.loadAnimation(v.getContext(),R.anim.rotate);
+        aeyoungBtn=v.findViewById(R.id.aeyoung_aeyoungcard);
+
+        aeyoungBtn.startAnimation(animation);
+
+        return v;
     }
 }
